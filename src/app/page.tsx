a2786590775e45b24fe8b61999b3553d01ea9609
prathambhalke <prathameshbhalke125@gmail.com>
2024-02-06 10:57:56 +0530

    changed the initial github user for better experience 🚀

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
-'use client'
+"use client";
 import DarkLightButton from "./components/DarkLightButton";
 import SearchAndButton from "./components/SearchAndButton";
-import Link from 'next/link'
-import Image from 'next/image'
+import Link from "next/link";
+import Image from "next/image";
 import { IoSearch, IoLocationOutline } from "react-icons/io5";
 import { GoOrganization } from "react-icons/go";
 import { IoIosLink } from "react-icons/io";
 import { FaTwitter } from "react-icons/fa";
-import {useQuery} from '@tanstack/react-query'
+import { useQuery } from "@tanstack/react-query";
 import dateFormat from "dateformat";
-import {useState} from "react" 
+import { useState } from "react";
 type GitHubUser = {
   avatar_url: string;
   name: string;
@@ -23,32 +23,31 @@ type GitHubUser = {
   blog: string;
   twitter_username: string;
   company: string;
-  profileNotFound : "";
-  documentation_url:"https://docs.github.com/rest/users/users#get-a-user";
-  message:"Not Found"
+  profileNotFound: "";
+  documentation_url: "https://docs.github.com/rest/users/users#get-a-user";
+  message: "Not Found";
 };
 
-
 export default function Home() {
-  const [user, setUser] = useState("prathambhalke");
+  const [user, setUser] = useState("");
   const { isLoading, error, data, refetch } = useQuery<GitHubUser>({
-    queryKey: ['repoData'],
+    queryKey: ["repoData"],
     queryFn: () =>
-      fetch(`https://api.github.com/users/${user}`).then((res) => res.json()),
+      fetch(`https://api.github.com/users/${user === "" ? "Octocat" : user}`).then((res) => res.json()),
   });
 
-  console.log("dd",data);
 
   const handleUserSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
     refetch();
   };
 
-  if(isLoading){
-    return(
-    <div className="flex w-full justify-center items-center h-screen">
-      <p className="animate-bounce">Loading...</p>
-      </div>)
+  if (isLoading) {
+    return (
+      <div className="flex w-full justify-center items-center h-screen">
+        <p className="animate-bounce">Loading...</p>
+      </div>
+    );
   }
 
   return (
@@ -59,7 +58,11 @@ export default function Home() {
           <DarkLightButton />
         </section>
         <section className="flex flex-col gap-6">
-          <SearchAndButton value={user} onChange={(e) => setUser(e.target.value)} onSubmit={handleUserSubmit} />
+          <SearchAndButton
+            value={user}
+            onChange={(e) => setUser(e.target.value)}
+            onSubmit={handleUserSubmit}
+          />
           {data?.message ? (
             <div className="flex w-full flex-col gap-6 rounded-lg bg-white dark:bg-slate-800 px-4 py-8 min-h/-[200px] text-red-500">
               <p className="text-red-500 text-center">No User Found</p>
@@ -70,18 +73,26 @@ export default function Home() {
               <section className="flex gap-2">
                 {/* image section */}
                 <Image
-  src={(data?.avatar_url || data?.documentation_url) ? data?.avatar_url ?? data?.documentation_url : ""}
-  className="rounded-full bg-white"
-  width={80}
-  height={80}
-  alt="profileImage"
-/>
+                  src={
+                    data?.avatar_url || data?.documentation_url
+                      ? data?.avatar_url ?? data?.documentation_url
+                      : ""
+                  }
+                  className="rounded-full bg-white"
+                  width={80}
+                  height={80}
+                  alt="profileImage"
+                />
 
                 <section className="flex flex-col justify-between gap-1 transition-all sm:w-full sm:flex-row">
                   <div>
                     {/* name */}
                     <h1>{data?.name}</h1>
-                    <Link target="_blank" href={`https://github.com/${data?.login}`} className="text-blue-500 text-sm hover:underline">
+                    <Link
+                      target="_blank"
+                      href={`https://github.com/${data?.login}`}
+                      className="text-blue-500 text-sm hover:underline"
+                    >
                       {data?.login}
                     </Link>
                     {/* user id */}
@@ -97,46 +108,72 @@ export default function Home() {
 
               {/* sec 2 */}
               <section className="flex flex-col gap-5">
-                <p>{data?.bio ?? <span className="opacity-60">This profile has no bio</span>}</p>
+                <p>
+                  {data?.bio ?? (
+                    <span className="opacity-60">This profile has no bio</span>
+                  )}
+                </p>
                 {/* repo and follower section */}
                 <div className="flex justify-between gap-3 rounded-lg bg-stone-100 px-6 py-4 dark:bg-slate-900 min-h-[50px]">
                   {/* item1 */}
                   <div className="flex flex-col items-center gap-2">
                     <p className="text-xs opacity-60">Repos</p>
-                    <p className="text-sm font-bold sm:text-base">{data?.public_repos}</p>
+                    <p className="text-sm font-bold sm:text-base">
+                      {data?.public_repos}
+                    </p>
                   </div>
                   {/* item2 */}
                   <div className="flex flex-col items-center gap-2">
                     <p className="text-xs opacity-60">Followers</p>
-                    <p className="text-sm font-bold sm:text-base">{data?.followers}</p>
+                    <p className="text-sm font-bold sm:text-base">
+                      {data?.followers}
+                    </p>
                   </div>
                   {/* item3 */}
                   <div className="flex flex-col items-center gap-2">
                     <p className="text-xs opacity-60">Following</p>
-                    <p className="text-sm font-bold sm:text-base">{data?.following}</p>
+                    <p className="text-sm font-bold sm:text-base">
+                      {data?.following}
+                    </p>
                   </div>
                 </div>
                 {/* address and extra links */}
                 <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 min-h-[47px]">
                   <div className="flex gap-2 items-center">
                     <IoLocationOutline className="text-xl" />
-                    <p>{data?.location ?? <span className="opacity-60"> Not Available</span>}</p>
-                  </div>
-                  <div className="flex gap-2 items-center">
-                    <IoIosLink className="text-xl"/>
-                    <Link target="_blank" href={data?.blog ?? "#"} className="hover:underline opacity-60 max-w-[200px] overflow-hidden text-ellipsis">
-                      {data?.blog ?? <span className="opacity-60"> Not Available</span>}
-                    </Link>
-                  </div>
-                  <div className="flex gap-2 items-center">
-                    <FaTwitter className="text-xl"/>
                     <p>
-                      {data?.twitter_username ?? <span className="opacity-60"> Not Available</span>}
+                      {data?.location ?? (
+                        <span className="opacity-60"> Not Available</span>
+                      )}
                     </p>
                   </div>
                   <div className="flex gap-2 items-center">
-                    <GoOrganization className="text-xl"/>
-                    <p>{data?.company ?? <span className="opacity-60"> Not Available</span>}</p>
+                    <IoIosLink className="text-xl" />
+                    <Link
+                      target="_blank"
+                      href={data?.blog ?? "#"}
+                      className="hover:underline opacity-60 max-w-[200px] overflow-hidden text-ellipsis"
+                    >
+                      {data?.blog ?? (
+                        <span className="opacity-60"> Not Available</span>
+                      )}
+                    </Link>
+                  </div>
+                  <div className="flex gap-2 items-center">
+                    <FaTwitter className="text-xl" />
+                    <p>
+                      {data?.twitter_username ?? (
+                        <span className="opacity-60"> Not Available</span>
+                      )}
+                    </p>
+                  </div>
+                  <div className="flex gap-2 items-center">
+                    <GoOrganization className="text-xl" />
+                    <p>
+                      {data?.company ?? (
+                        <span className="opacity-60"> Not Available</span>
+                      )}
+                    </p>
                   </div>
                 </div>
               </section>
